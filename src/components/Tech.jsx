38a0c071888336f352a7e2ccb88526c1d5f06989
Mominuddin07 <mohammedmominuddin07@gmail.com>
2025-02-28 03:57:57 +0530

    again

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import { BallCanvas } from "./canvas";
 import SectionWrapper from "../hoc/SectionWrapper";
 import { technologies } from "../constants";
@@ -8,14 +7,17 @@ const Tech = () => {
   return (
     <div>
       <h2
-  className="text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px] text-center mb-8"
->
-  Skills
-</h2>
+        className="text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px] text-center mb-8"
+      >
+        Skills
+      </h2>
 
-      <div className="flex flex-row flex-wrap justify-center gap-10">
+      <div className="flex flex-wrap justify-center gap-6 sm:gap-8 md:gap-10">
         {technologies.map((technology) => (
-          <div className="w-28 h-28" key={technology.name}>
+          <div 
+            className="w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 lg:w-28 lg:h-28"
+            key={technology.name}
+          >
             <BallCanvas icon={technology.icon} />
           </div>
         ))}
