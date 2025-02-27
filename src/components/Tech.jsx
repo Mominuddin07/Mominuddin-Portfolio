2678b85cd500b605b06cc0b83bee7cfc40c1370d
Mominuddin07 <mohammedmominuddin07@gmail.com>
2025-02-28 04:28:42 +0530

    again

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -6,19 +6,25 @@ import { technologies } from "../constants";
 const Tech = () => {
   return (
     <div>
-      <h2
-        className="text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px] text-center mb-8"
-      >
+      <h2 className="text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px] text-center mb-8">
         Skills
       </h2>
 
       <div className="flex flex-wrap justify-center gap-6 sm:gap-8 md:gap-10">
-        {technologies.map((technology) => (
-          <div 
-            className="w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 lg:w-28 lg:h-28"
+        {technologies.map((technology, index) => (
+          <div
             key={technology.name}
+            className="w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 lg:w-28 lg:h-28 flex items-center justify-center"
           >
-            <BallCanvas icon={technology.icon} />
+            {index < 5 ? (
+              // Display first 5 technologies as Three.js balls
+              <BallCanvas icon={technology.icon} />
+            ) : (
+              // Display other technologies as normal icons in gray circles
+              <div className="w-full h-full bg-gray-800 rounded-full flex items-center justify-center p-3">
+                <img src={technology.icon} alt={technology.name} className="w-3/4 h-3/4 object-contain" />
+              </div>
+            )}
           </div>
         ))}
       </div>
